d26ad1f264821dfda7842a767b3f2e6ecc3cde55
Troed Sångberg <github@troed.se>
2026-08-15 21:03:46 +0200

    feat(serve): add the weight residency option

--- src/serve/serve_options.h
+++ src/serve/serve_options.h
@@ -38,6 +38,7 @@ struct ServeOptions {
     std::size_t response_store_max_bytes   = kDefaultResponseStoreBytes;
     int device                             = 0;
     KvCacheStorage kv_cache                = KvCacheStorage::BFloat16;
+    WeightResidency weight_residency       = WeightResidency::AllResident;
     SpeculativeOptions speculative;
     bool enable_vision      = false;
     bool use_cuda_graph     = true;
